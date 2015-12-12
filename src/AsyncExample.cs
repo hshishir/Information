889a9dfba383fb54d42041fb674aeee2f6a17922
Hemang Shishir <hshishir@microsoft.com>
2015-12-11 23:00:21 -0800

    Add async programming example

---
+++ src/AsyncExample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Information
+{
+	public class AsyncExample
+	{
+		public async Task<int> GetWebAsync(string url)
+		{
+			var httpClient = new HttpClient();
+			Task<string> getStringTask = httpClient.GetStringAsync(url);
+			string content = await getStringTask;
+			return content.Length;
+		}
+	}
+}
---
+++ src/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+
+namespace Information
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TestAsyncExample();
+
+			Console.WriteLine("Carry on");
+		}
+
+		static async void TestAsyncExample()
+		{
+			var obj = new AsyncExample();
+			var url = "http://www.microsoft.com";
+
+			int contentLength = await obj.GetWebAsync(url);
+			Console.WriteLine("Url={0}, Length={1}", url, contentLength);
+		}
+	}
+}
